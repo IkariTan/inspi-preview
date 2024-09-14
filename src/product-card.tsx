658b0982f7fb78583b7c8812df7f5a306d5bdc7b
Gourab Nag <gourabnag12@gmail.com>
2024-09-14 11:38:37 +0530

    feat: load compressed images for product card

--- src/product-card.tsx
+++ src/product-card.tsx
@@ -1,5 +1,50 @@
 import { useEffect, useState } from "react";
 
+export function getImageUrl(path: string, width: string) {
+  let img_url: string = path?.replace(
+    "materialdepotimages.s3.ap-south-1.amazonaws.com",
+    "dpy2z8n9cxui1.cloudfront.net"
+    // `materialdepotimages-compressed-${percent}-percent`
+  );
+
+  img_url = img_url?.replaceAll(
+    "materialdepotimages.s3.amazonaws.com",
+    "dpy2z8n9cxui1.cloudfront.net"
+  );
+
+  img_url = img_url?.replaceAll(
+    "material-depot-content-files.s3.ap-south-1.amazonaws.com",
+    "dqzffhb3lxxp.cloudfront.net"
+  );
+
+  if (
+    img_url?.includes(".mp4") ||
+    img_url?.includes(".mov") ||
+    img_url?.includes(".gif")
+  ) {
+    img_url = img_url?.replaceAll(
+      "dpy2z8n9cxui1.cloudfront.net",
+      "d2cwt1uuomj2h5.cloudfront.net"
+    );
+  }
+
+  if (img_url) {
+    if (img_url[0] === "/") {
+      img_url = "https://dpy2z8n9cxui1.cloudfront.net" + img_url;
+    }
+  }
+
+  img_url = img_url?.replaceAll("+", "%20");
+  if (!img_url?.startsWith("https")) {
+    img_url = "https://dpy2z8n9cxui1.cloudfront.net" + img_url;
+  }
+  if (width !== "") {
+    img_url += "?width=" + width;
+  }
+
+  return img_url;
+}
+
 export function ProductCard({ variant_handle }: { variant_handle: string }) {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const [productData, setProductData] = useState<any | null>(null);
@@ -25,7 +70,10 @@ export function ProductCard({ variant_handle }: { variant_handle: string }) {
       }}
     >
       <img
-        src={productData?.data?.variant_image?.[0]?.image_url}
+        src={getImageUrl(
+          productData?.data?.variant_image?.[0]?.image_url,
+          "200"
+        )}
         className="w-full h-auto object-contain rounded bg-gray-200"
         style={{ aspectRatio: 0.75 }}
       />
